968749aa8eaba90392985668e3ade873c301bf98
tsubametsai <36106518+tsubametsai@users.noreply.github.com>
2018-04-07 18:31:18 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,44 +1,44 @@
-資料庫（reshape2）
-文件名 < -  “ getdata_dataset.zip ”
-＃＃下載並解壓縮數據集：
+library（reshape2）
+filename < -  “ getdata_dataset.zip ”
+＃＃Download and unzip the dataset
 if（！ file.exists（filename））{
 fileURL  < -  “ https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ”
-  download.file（fileURL，文件名，method = “ curl ”）
+  download.file（fileURL，filename，method = “ curl ”）
 } 
 if（！ file.exists（“ UCI HAR Dataset ”））{
-解壓縮（文件名）
+unzip(filename) 
 }
 
-＃加載活動標籤+功能
+＃Load activity labels + features
 activityLabels  < - read.table（“ UCI HAR Dataset / activity_labels.txt ”）
 activityLabels [，2 ] < - as.character（activityLabels [，2 ]）
 features  < - read.table（“ UCI HAR Dataset / features.txt ”）
-特徵 [，2 ] < - as.character（特徵 [，2 ]）
+features[，2 ] < - as.character（features [，2 ]）
 
-＃僅提取平均值和標準偏差的數據
+＃Extract only the data on mean and standard deviation
 featuresWanted  < - grep（“。* mean。* |。* std。* ”，features [，2 ]）
-featuresWanted.names  < -  功能 [ featuresWanted，2 ]
+featuresWanted.names  < -  features [ featuresWanted，2 ]
 featuresWanted.names  = gsub（'- mean '，' Mean '，featuresWanted.names）
-featuresWanted.names  = GSUB（' -std '，'標準'，featuresWanted.names）
+featuresWanted.names  = GSUB（' -std '，'std'，featuresWanted.names）
 featuresWanted.names  < - gsub（' [ - （）] '，' '，featuresWanted.names）
 
-＃加載數據集
-火車 < - read.table（“ UCI HAR數據集/ train / X_train.txt ”）[ featuresWanted ]
-trainActivities  < - read.table（“ UCI HAR數據集/火車/ Y_train.txt ”）
+＃Load the datasets
+train < - read.table（“ UCI HAR Dataset/ train / X_train.txt ”）[ featuresWanted ]
+trainActivities  < - read.table（“ UCI HAR Dataset/train/ Y_train.txt ”）
 trainSubjects  < - read.table（“ UCI HAR Dataset / train / subject_train.txt ”）
-火車 < - cbind（trainSubjects，trainActivities，train）
-測試 < - read.table（“ UCI HAR數據集/測試/ X_test.txt ”）[ featuresWanted ]
+train < - cbind（trainSubjects，trainActivities，train）
+text < - read.table（“ UCI HAR Dataset/text/ X_test.txt ”）[ featuresWanted ]
 testActivities  < - read.table（“ UCI HAR Dataset / test / Y_test.txt ”）
 testSubjects  < - read.table（“ UCI HAR Dataset / test / subject_test.txt ”）
-測試 < - cbind（testSubjects，testActivities，test）
+text < - cbind（testSubjects，testActivities，test）
 
-＃合併數據集並添加標籤
+＃merge datasets and add labels
 allData  < - rbind（train，test）
 colnames（allData）< - c（“ subject ”，“ activity ”，featuresWanted.names）
 
-＃將活動和主題轉化為因素
+＃turn activities & subjects into factors
 allData $ activity  < -  factor（allData $ activity，levels  =  activityLabels [，1 ]，labels  =  activityLabels [，2 ]）
 allData $ subject  < - as.factor（allData $ subject）
 allData.melted  < - melt（allData，id  = c（“ subject ”，“ activity ”））
-allData.mean  < - dcast（allData.melted，受試者 +  活動 〜 變量，平均）
+allData.mean  < - dcast（allData.melted，subject +  activity 〜 variable，mean）
 write.table（allData.mean，“ tidy.txt ”，row.names  =  FALSE，quote  =  FALSE）
